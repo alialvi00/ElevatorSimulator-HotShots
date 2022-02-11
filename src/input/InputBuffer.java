031fbca9ebc93ec5229e2da913704b28ba8eb463
Akaash Kapoor <akaash.kapoor@carleton.ca>
2022-02-10 21:22:15 -0500

    doc: add javadoc to Reader and InputBuffer class

--- src/input/InputBuffer.java
+++ src/input/InputBuffer.java
@@ -3,20 +3,45 @@ package input;
 
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
+
 
 /**
  * @author Akaash Kapoor
  *
  */
+
+/**
+ * This class acts as an intermediate between the Reader 
+ * and the FloorSubsystem classes by storing and retrieving
+ * text data. 
+ * @author Akaash Kapoor
+ *
+ */
 public class InputBuffer {
 	
+	/**The linked blocking queue buffer.*/
 	private LinkedBlockingQueue<ArrayList<String>> buffer;
 	
+	/**
+	 * Constructor for InputBuffer class. Instantiates blocking queue. 
+	 */
 	public InputBuffer() {
 		buffer = new LinkedBlockingQueue<>();
 	}
 	
+	/**
+	 * Getter function that returns the data inside the queue. 
+	 * @return an ArrayList of the data in the queue. 
+	 */
+	public ArrayList<String> getDataFromInputBuffer(){
+		return buffer.peek();
+	}
+	
+	/**
+	 * This method inputs data into the buffer. 
+	 * @param data - The data needed to be inserted and passed to the 
+	 * floor subsystem.
+	 */
 	public void sendToInputBuffer(ArrayList<String> data){
         try {
             buffer.put(data);
@@ -26,6 +51,10 @@ public class InputBuffer {
         }
     }
     
+	/**
+	 * This method extracts data from the buffer. 
+	 * @return An array list that contains stored data. 
+	 */
     public ArrayList<String> recieveFromInputBuffer(){
     	
     	ArrayList<String> data = new ArrayList<>();
--- src/input/Reader.java
+++ src/input/Reader.java
@@ -1,38 +1,58 @@
 package input;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-import floorSubsystem.FloorSubsystem;
-
+/**
+ * This class reads the text input and sends it to the
+ * Input Buffer class. 
+ * @author Akaash Kapoor
+ *
+ */
 public class Reader implements Runnable {
-    private BufferedReader reader_file;
+	
+	/**Instance variable for reading the input file.*/
+    private BufferedReader readerFile;
+    /**Buffer for storing input data read from text file. */
     private InputBuffer buffer;
+    /**Keeps track of the lines read in the input file. */
     private static int lineCounter;
 
+    /**
+     * Constructor for initializing Reader Object
+     * @param buffer A blocking queue used to send data to the Floor Subsystem.
+     */
     public Reader(InputBuffer buffer){
     	
     	this.buffer = buffer;
         lineCounter = 0;
         try{
-            this.reader_file = new BufferedReader(new FileReader("Inputs/test.txt"));
+            this.readerFile = new BufferedReader(new FileReader("Inputs/test.txt"));
         } catch(IOException e){
             System.exit(0);
         }
     }
-
+    
+    /**
+     * Getter function for extracting line counter. 
+     * @return lineCounter
+     */
     public static int getLineCounter(){
         return lineCounter;
     }
     
+    /**
+     * Runs the Reader thread. 
+     */
     @Override
     public void run() {
 
         try{
-            while(reader_file.ready())
+            while(readerFile.ready())
                 readInput();
         } catch (IOException e){
             e.printStackTrace();
@@ -40,12 +60,18 @@ public class Reader implements Runnable {
 
     }
     
+    /**
+     * Helper function to read input. 
+     * @throws IOException This is thrown if reader cannot read file. 
+     */
     public void readInput() throws IOException {
     	ArrayList<String> dataInfo = new ArrayList<>();
-    	StringTokenizer st = new StringTokenizer(reader_file.readLine(), " ");
-        for(int i = 0; i < 4; i++)
+    	StringTokenizer st = new StringTokenizer(readerFile.readLine(), " ");
+        
+    	for(int i = 0; i < 4; i++)
             dataInfo.add(st.nextToken());
-
+    	
+    	//Inputs info into the InputBuffer.
         buffer.sendToInputBuffer(dataInfo);
         lineCounter++;
     }
