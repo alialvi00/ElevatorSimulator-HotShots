aeadbd843c82ab4602f377e460ded005637f8583
H-Jallad <hassan.jallad@hotmail.com>
2022-02-19 03:51:08 -0500

    added state machine code to the elevator sub system

--- src/elevatorSubsystem/ElevatorSubsystem.java
+++ src/elevatorSubsystem/ElevatorSubsystem.java
@@ -5,6 +5,163 @@ import scheduler.*;
 
 import java.util.ArrayList;
 
+abstract class State {
+
+    protected ElevatorSubsystem elevatorSubsystem;
+
+    static State current, stationary, movingUp, movingDown;
+
+    public State(ElevatorSubsystem elevator){
+        this.elevatorSubsystem = elevator;
+    }
+
+    abstract void enterState();
+    abstract void updateState();
+}
+
+/**
+ * this class describes the elevator in a stationary state
+ */
+class Stationary extends State{
+
+    public Stationary(ElevatorSubsystem elevatorSubsystem){
+        super(elevatorSubsystem);
+    }
+
+    void enterState() {
+        if (elevatorSubsystem.isMotorOn()){
+            elevatorSubsystem.setElevatorDoors(true); //open door
+            elevatorSubsystem.setMotor(false); //turn off motor
+        }
+        System.out.println("Elevator is at a stationary state");
+        elevatorSubsystem.updateRequest();
+    }
+
+    void updateState(){
+        //request received from scheduler
+        SchedulerRequest request = elevatorSubsystem.getRequests();
+
+        if (request.getDestinationFloor() == request.getCurrentFloor()){
+            current = stationary;
+            return;
+        }
+        else if (request.getDestinationFloor() > request.getCurrentFloor()){
+            current = movingUp;
+            return;
+        }
+        else if (request.getDestinationFloor() < request.getCurrentFloor()){
+            current = movingDown;
+            return;
+        }
+    }
+}
+
+/**
+ * class that puts elevator in the state of MovingUp
+ */
+class MovingUp extends State{
+
+    public MovingUp(ElevatorSubsystem elevatorSubsystem){
+        super(elevatorSubsystem);
+    }
+
+    void enterState(){
+        if (!elevatorSubsystem.isMotorOn()){
+            elevatorSubsystem.setElevatorDoors(false);
+            elevatorSubsystem.setMotor(true); //turn on motor
+        }
+        System.out.println("Elevator is moving one floor up");
+
+        //simulating elevator moving
+        try {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    void updateState(){
+        SchedulerRequest request = elevatorSubsystem.getRequests();
+
+        //update current floor
+        request.setCurrentFloor(request.getCurrentFloor() + 1);
+
+        if (request.getDestinationFloor() > request.getCurrentFloor()){
+            current = movingUp;
+            return;
+        }
+        if (request.getDestinationFloor() == request.getCurrentFloor()){
+            //sending new data to scheduler
+            elevatorSubsystem.sendToScheduler(request);
+
+            //wait for potential response
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            current = stationary;
+            return;
+        }
+    }
+}
+
+/**
+ * class that puts elevator in the state of MovingDown
+ */
+class MovingDown extends State{
+
+    public MovingDown(ElevatorSubsystem elevatorSubsystem){
+        super(elevatorSubsystem);
+    }
+
+    void enterState(){
+        if (!elevatorSubsystem.isMotorOn()){
+            elevatorSubsystem.setElevatorDoors(false); //close doors
+            elevatorSubsystem.setMotor(true); //turn on motor
+        }
+        System.out.println("Elevator is moving one floor down");
+
+        //simulating elevator moving
+        try {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    void updateState(){
+        SchedulerRequest request = elevatorSubsystem.getRequests();
+
+        //update current floor
+        request.setCurrentFloor(request.getCurrentFloor() - 1);
+
+        if (request.getDestinationFloor() < request.getCurrentFloor()){
+            current = movingDown;
+            return;
+        }
+        if (request.getDestinationFloor() == request.getCurrentFloor()){
+            //sending new data to scheduler
+            elevatorSubsystem.sendToScheduler(request);
+
+            //waiting for potential response
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            current = stationary;
+            return;
+        }
+    }
+}
+
+
 public class ElevatorSubsystem implements Runnable {
 
     private Scheduler buf;
@@ -37,32 +194,25 @@ public class ElevatorSubsystem implements Runnable {
         upDirectionLamp = false;
         downDirectionLamp = false;
         numOfElevators++;
+        scheduledRequestsRequest = new SchedulerRequest();
     }
 
     @Override
     public void run() {
-    	
-        while(counter < Reader.getLineCounter()){
+        //initiating all the states
+        State.stationary = new Stationary(this);
+        State.movingUp = new MovingUp(this);
+        State.movingDown = new MovingDown(this);
+        State.current = State.stationary;   //we start at stationary
 
-            scheduledRequestsRequest = buf.recieveFromScheduler("elevator");
-            System.out.println(Thread.currentThread().getName() + " has pulled " + scheduledRequestsRequest + " from Scheduler.");
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            }
-
-            System.out.println(Thread.currentThread().getName() + " is sending " + scheduledRequestsRequest + " to Scheduler.");
-            buf.sendToScheduler(scheduledRequestsRequest, "elevator");
-
-            counter++;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
-                e.printStackTrace();
-            }
-
+        //how this loop runs and exists is temporary for now
+        //until we agree on the proper way
+        //setting a timer for testing purposes
+        long start = System.currentTimeMillis();
+        long end = start + 30*1000;
+        while (System.currentTimeMillis() < end) {
+            State.current.enterState();
+            State.current.updateState();
         }
     }
     
@@ -76,7 +226,7 @@ public class ElevatorSubsystem implements Runnable {
 
 
     /**
-     * @param moving elevator to moving or not
+     * @param moving elevator to moving (true) or not (false)
      */
     public void setMoving(boolean moving) {
         this.moving = moving;
@@ -92,7 +242,7 @@ public class ElevatorSubsystem implements Runnable {
 
 
     /**
-     * @param status elevator doors to open or closed
+     * @param status elevator doors to open (true) or closed (false)
      */
     public void setElevatorDoors(boolean status) {
         this.elevatorDoors = status;
@@ -141,5 +291,30 @@ public class ElevatorSubsystem implements Runnable {
         return numOfElevators;
     }
 
+    /**
+     * this method updates
+     */
+    public void updateRequest(){
+        this.scheduledRequestsRequest = buf.recieveFromScheduler("elevator");
+        System.out.println(Thread.currentThread().getName() + " has pulled " + scheduledRequestsRequest + " from Scheduler.");
+    }
+
+    /**
+     * returns the request
+     * @return of type SchedulerRequest
+     */
+    public SchedulerRequest getRequests(){
+        return scheduledRequestsRequest;
+    }
+
+    /**
+     * this method sends new data to scheduler
+     * @param data of type SchedulerRequest
+     */
+    public void sendToScheduler(SchedulerRequest data){
+        this.buf.sendToScheduler(data, "elevator");
+        this.buf.elevatorArrived("place holder", data.getCurrentFloor(), data.getDestinationFloor());
+        System.out.println(Thread.currentThread().getName() + " is sending " + scheduledRequestsRequest + " to Scheduler.");
+    }
 
 }
