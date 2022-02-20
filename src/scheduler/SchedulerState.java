397b99161e976128b1a826b6526ad08759750406
raj1034 <rajsandhu@cmail.carleton.ca>
2022-02-19 22:20:25 -0500

    Cleaned up classes in scheduler subsystem.

--- src/scheduler/SchedulerState.java
+++ src/scheduler/SchedulerState.java
@@ -1,32 +1,42 @@
 package scheduler;
 
+/**
+ * The following enum represents the states the scheduler can take.
+ * @author Ali Alvi Raj Sandhu
+ * @version 1.0
+ *
+ */
+
 public enum SchedulerState {
 	
+	//If the state is idle switch to waiting state.
 	IDLE {
 		
 		@Override
 		public SchedulerState transition(Event e) {
 			if(e == Event.RECEIVED_REQUEST) {
 
-				return WAITING;
+				return WAITING; //State is waiting.
 
 			}
 			return ERROR;
 		}
 	},
 	
-
+	//If the state is waiting switch to idle state.
 	WAITING {
 
 		
 		@Override
 		public SchedulerState transition(Event e) {
 			if(e == Event.SCHEDULED_REQUEST) {
-				return IDLE;
+				return IDLE; //State is Idle.
 			}
 			return ERROR;
 		}
 	},
+	
+	//Go to error state if there is an error.
 	ERROR{
 		
 		@Override
@@ -35,9 +45,15 @@ public enum SchedulerState {
 		}
 	};
 	
+	/**
+	 * Abstract class which represents the state transitions for the scheduler.
+	 * @param e the event transition.
+	 * @return scheduler state.
+	 */
 	
 	public abstract	SchedulerState transition(Event e);
 	
+	//Events which can cause a change of states.
 	public static enum Event{
 		RECEIVED_REQUEST,
 		SCHEDULED_REQUEST
