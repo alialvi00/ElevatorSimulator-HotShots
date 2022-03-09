0e72cfd062916a07611c4582f924da44d712b2cf
alialvi00 <ali_mashhood98@hotmail.com>
2022-03-08 22:31:47 -0500

    Removed SchRequest and Updated Sch
    Removed SchRequest and Updated Sch

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,10 +1,16 @@
 package scheduler;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import elevatorSubsystem.ElevatorRequest;
+import floorSubsystem.FloorRequest;
 import scheduler.SchedulerState.Event;
 
 /**
@@ -14,38 +20,139 @@ import scheduler.SchedulerState.Event;
  * @version 1.0
  */
 
-public class Scheduler extends Thread{
-	
-    private LinkedBlockingQueue<SchedulerRequest> buffer;
-    private SchedulerRequest schRequest;
+public class Scheduler implements Runnable{
+
     private SchedulerStateMachine fsm;
     
     InetAddress elevatorAddress;
     InetAddress floorAddress;
     
     private int floorPort, elevatorPort;
+    private int floors, numElevators;
+    private FloorRequest floorRequest;
+    private ElevatorRequest elevatorRequest;
+    
+    private boolean systemOnline;
     
     DatagramSocket sendSocket, receiveSocket;
     DatagramPacket receivePacket, floorPacket, elevatorPacket;
 
     ArrayList<DatagramPacket> receiveRequests;
     ArrayList<ElevatorRequest> elevatorRequests;
+    PriorityQueue<FloorRequest> floorRequests;
     
     /**
      * Create the scheduler constructor.
      */
-    public Scheduler(){
-        buffer = new LinkedBlockingQueue<>();
-        schRequest = new SchedulerRequest();
+    public Scheduler(int floors, int numElevators){
+    	
         fsm = new SchedulerStateMachine();
+        this.floors = floors;
+        this.numElevators = numElevators;
+        
+        receiveRequests = new ArrayList<>();
+        elevatorRequests = new ArrayList<>();
+        floorRequests = new PriorityQueue<>();
+        
+        try {
+        	elevatorAddress = InetAddress.getLocalHost();
+        	floorAddress = InetAddress.getLocalHost();
+        }
+        catch(UnknownHostException he) {
+        	he.printStackTrace();
+        	System.exit(1);
+        }
+        
+        try {
+        	sendSocket = new DatagramSocket();
+        	receiveSocket = new DatagramSocket(69);
+        }
+        catch(SocketException se) {
+        	se.printStackTrace();
+        	System.exit(1);
+        }
+        
+        systemOnline = false;
+        
+        setUpElevators();
     }
+    
+    
+    public void setUpElevators() {
+    	for(int i =0; i<numElevators; i++) {
+    		elevatorRequests.add(new ElevatorRequest(i+1));
+    	}
+    	systemOnline = true;
+    }
+    
+    public void handleRequests() {
+    	
+    	receivePacket = new DatagramPacket(new byte[700], 700);
+    	
+    	try {
+    		receiveSocket.receive(receivePacket);
+    		receiveRequests.add(receivePacket);
+    	}
+    	catch(IOException ie) {
+    		ie.printStackTrace();
+    		System.exit(1);
+    	}
+    }
+    
+    public void createSendPacket() {
+    	
+    	if(!receiveRequests.isEmpty()) {
+    		
+    		for(DatagramPacket eachRequest: receiveRequests) {
+    			
+    			Object currentRequest = bytesToPacket(eachRequest);
+    			
+    			if(currentRequest instanceof ElevatorRequest) {
+    				
+    				elevatorRequest = (ElevatorRequest)currentRequest;
+    			}
+    			
+    			else if(currentRequest instanceof FloorRequest) {
+    				
+    				floorRequest = (FloorRequest)currentRequest;
+    				floorAddress = eachRequest.getAddress();
+    				floorPort = eachRequest.getPort();
+    		}
+    	}
+    }
+    
+    public Object bytesToPacket(DatagramPacket request) {
+    	
+    	ByteArrayInputStream inputStream = new ByteArrayInputStream(request.getData());
+    	ObjectInputStream objectStream = null;
+    	Object requestObject = null;
+    	
+    	try {
+			new ObjectInputStream(new BufferedInputStream(inputStream));
+			requestObject = objectStream.readObject();
+			objectStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		} catch(ClassNotFoundException ce) {
+			ce.printStackTrace();
+			System.exit(1);
+		}
+    	return requestObject;
+    }
+    
     
     /**
      * Method which gets scheduler request.
      * @return scheduler request blocking queue.
      */
-    public LinkedBlockingQueue<SchedulerRequest> getBuffer(){
-    	return buffer;
+    public LinkedBlockingQueue<FloorRequest> getFloorBuffer(){
+    	return floorBuffer;
+    }
+    
+    public LinkedBlockingQueue<ElevatorRequest> getElevatorBuffer(){
+    	return elevatorBuffer;
     }
     
     /**
@@ -124,6 +231,17 @@ public class Scheduler extends Thread{
      */
     public LinkedBlockingQueue<SchedulerRequest> getRequestQue() {
 		return buffer;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(systemOnline) {
+			handleRequests();
+			createSendPacket();
+			
+			Thread.sleep(250);
+		}
 	}
     
 }
